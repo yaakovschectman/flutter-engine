05c747bd665004d8f4c06f80d5981d0b78e893f6
schectman <schectman@google.com>
2022-11-17 15:32:11 -0500

    Initial commit

---
+++ third_party/accessibility/ax/platform/ax_platform_node_textrangeprovider_win.h
@@ -0,0 +1,10 @@
+// Copyright 2019 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_ACCESSIBILITY_PLATFORM_AX_PLATFORM_NODE_TEXTRANGEPROVIDER_WIN_H_
+#define UI_ACCESSIBILITY_PLATFORM_AX_PLATFORM_NODE_TEXTRANGEPROVIDER_WIN_H_
+
+
+
+#endif  // UI_ACCESSIBILITY_PLATFORM_AX_PLATFORM_NODE_TEXTRANGEPROVIDER_WIN_H_
